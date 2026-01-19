5249839f659d59c2e7e7ccd4d25b403dc5e940a1
tangerineshirt <razzan.rianta@gmail.com>
2026-01-19 21:40:22 +0700

    added lots of things

---
+++ basic-oop/graph.py
@@ -0,0 +1,9 @@
+class Node:
+    def __init__(self, data, next):
+        self.data = data
+        self.next = next
+
+class Graph:
+    def __init__(self, amount):
+        self.jNode = amount
+        
